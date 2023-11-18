c6b4ecbc122ca895b5eea1dc7a5acf43a793c7a7
Allan Rolle <allanr@MacBook-Air-de-Allan.local>
2023-11-18 19:44:58 +0100

    algo

---
+++ 1. Fichiers de départ/9.LargestValues/script.js
@@ -0,0 +1,26 @@
+function largestValues(arr){
+  return arr.reduce((acc, curr) => {
+    acc.push(Math.max(...curr))
+    return acc;
+  }, [])
+}
+
+
+/* ÉNONCÉ 📚 */
+
+
+/* 
+Créez un Algorithme qui prend un tableau composé de plusieurs tableaux en argument et qui retourne un seul tableau qui contient la valeur maximale de chaque sous-tableau. 
+*/
+
+
+/* Test à passer 🧪 */
+
+console.log(
+  largestValues([
+    [-1, -5, -8, 0],
+    [15, 47, 88, 26],
+    [32, 35, 37, 39],
+    [3000, 1001, 857, 1],
+  ])
+); // [ 0, 88, 39, 3000 ]
